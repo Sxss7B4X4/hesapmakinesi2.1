4fc2d84d0e679a0a981eee554c0d3262dcc80d78
Sxss7B4X4 <98440714+Sxss7B4X4@users.noreply.github.com>
2022-01-26 06:12:28 +0300

    Add files via upload

---
+++ Hesap_Makinesi_v2.1.py
@@ -0,0 +1,16 @@
+﻿import time
+print ("Yapılacak Matematik İşlemini Seçiniz")
+
+print ("Çarpma İçin 1 Toplama İçin 2")
+
+secim=input("Seçiminiz Nedir 1-2 : ")
+
+sayi1 = int(input("1.ci Sayı : "))
+sayi2 = int(input("2.ci Sayı : "))
+if secim =="1":
+    print(sayi1, "*" ,sayi2,)
+print (sayi1 * sayi2)
+if secim=="2":
+    print(sayi1, "+" ,sayi2, "=" ,sayi1+sayi2)
+time.sleep(3)
+# Made By Memati Baş#4472
